a1e6a59a145fb3c00f302d8c54ad0919e9d6723d
NataliaDrause <nataliadrause@gmail.com>
2022-12-09 10:04:48 -0500

    add custom style to the plugin setting page

--- wp-content/plugins/our-word-filter-plugin/index.php
+++ wp-content/plugins/our-word-filter-plugin/index.php
@@ -21,14 +21,29 @@ class OurWordFilterPlugin {
   // Register menu options.
   function our_menu() {
     //add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array ( $this, 'word_filter_page'), plugin_dir_url(__FILE__) . 'custom.svg', 100);
-    add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array ( $this, 'word_filter_page'), 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMCAyMEMxNS41MjI5IDIwIDIwIDE1LjUyMjkgMjAgMTBDMjAgNC40NzcxNCAxNS41MjI5IDAgMTAgMEM0LjQ3NzE0IDAgMCA0LjQ3NzE0IDAgMTBDMCAxNS41MjI5IDQuNDc3MTQgMjAgMTAgMjBaTTExLjk5IDcuNDQ2NjZMMTAuMDc4MSAxLjU2MjVMOC4xNjYyNiA3LjQ0NjY2SDEuOTc5MjhMNi45ODQ2NSAxMS4wODMzTDUuMDcyNzUgMTYuOTY3NEwxMC4wNzgxIDEzLjMzMDhMMTUuMDgzNSAxNi45Njc0TDEzLjE3MTYgMTEuMDgzM0wxOC4xNzcgNy40NDY2NkgxMS45OVoiIGZpbGw9IiNGRkRGOEQiLz4KPC9zdmc+', 100);
+    $main_page_hook = add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array ( $this, 'word_filter_page'), 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMCAyMEMxNS41MjI5IDIwIDIwIDE1LjUyMjkgMjAgMTBDMjAgNC40NzcxNCAxNS41MjI5IDAgMTAgMEM0LjQ3NzE0IDAgMCA0LjQ3NzE0IDAgMTBDMCAxNS41MjI5IDQuNDc3MTQgMjAgMTAgMjBaTTExLjk5IDcuNDQ2NjZMMTAuMDc4MSAxLjU2MjVMOC4xNjYyNiA3LjQ0NjY2SDEuOTc5MjhMNi45ODQ2NSAxMS4wODMzTDUuMDcyNzUgMTYuOTY3NEwxMC4wNzgxIDEzLjMzMDhMMTUuMDgzNSAxNi45Njc0TDEzLjE3MTYgMTEuMDgzM0wxOC4xNzcgNy40NDY2NkgxMS45OVoiIGZpbGw9IiNGRkRGOEQiLz4KPC9zdmc+', 100);
     add_submenu_page('ourwordfilter', 'Words to Filter', 'Words List', 'manage_options', 'ourwordfilter', array($this, 'word_filter_page'));
     add_submenu_page('ourwordfilter', 'Word Filter Options', 'Options', 'manage_options', 'word-filter-options', array($this, 'options_subpage'));
+    // Add CSS to the settings page.
+    add_action("load-{$main_page_hook}", array($this, 'main_page_assets'));
+  }
+
+  function main_page_assets() {
+    wp_enqueue_style('filter_admin_CSS', plugin_dir_url(__FILE__) . 'styles.css');
   }
 
   // HTML for main settings page.
   function word_filter_page() { ?>
-    Hello world
+    <div class="wrap">
+      <h1>Word Filter</h1>
+      <form method="POST">
+        <label for="plugin_words_to_filter"><p>Enter a <strong>comma-separated</strong> list of words to filter from your site's content.</p></label>
+        <div class="word-filter__flex-container">
+          <textarea name="plugin_words_to_filter" id="plugin_words_to_filter" placeholder="bad, mean, aweful, horrible"></textarea>
+        </div>
+        <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+      </form>
+    </div>
   <?php }
 
   // HTML for settings subpage.
