44ec795d30ea16b616d7c69393a671e1c6748336
NataliaDrause <nataliadrause@gmail.com>
2023-01-30 18:59:41 +0200

    control post meta with block type and add related posts to the CPT

--- wp-content/plugins/featured-professor/featured-professor.php
+++ wp-content/plugins/featured-professor/featured-professor.php
@@ -10,17 +10,25 @@
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 require_once plugin_dir_path(__FILE__) . 'inc/generateProfessorHTML.php';
+require_once plugin_dir_path(__FILE__) . 'inc/relatedPostsHTML.php';
 
 class FeaturedProfessor {
   function __construct() {
     add_action('init', [$this, 'onInit']);
     // Add custom REST API for professor HTML:
     add_action('rest_api_init', [$this, 'profHTML']);
+    // Add posts list to the Professor CPT:
+    add_filter('the_content', [$this, 'addRelatedPosts']);
   }
 
   function onInit() {
     wp_register_script('featuredProfessorScript', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-i18n', 'wp-editor'));
     wp_register_style('featuredProfessorStyle', plugin_dir_url(__FILE__) . 'build/index.css');
+    register_meta('post', 'featuredprofessor', array(
+      'show_in_rest' => true,
+      'type' => 'number',
+      'single' => false,
+    ));
 
     register_block_type('ourplugin/featured-professor', array(
       'render_callback' => [$this, 'renderCallback'],
@@ -49,6 +57,15 @@ class FeaturedProfessor {
 
   function getProfHTML($data) {
     return generateProfessorHTML($data['profId']);
+  }
+
+  // Add posts list to the Professor CPT:
+
+  function addRelatedPosts($content) {
+    if (is_singular('professor') && in_the_loop() && is_main_query()) {
+      return $content . relatedPostsHTML(get_the_id());
+    }
+    return $content;
   }
 
 }
