f2a843f30576c379d913cb0c1b16104b63fa9604
NataliaDrause <nataliadrause@gmail.com>
2023-02-27 16:31:02 +0200

    customize theme in theme.json

--- wp-content/themes/fictional-block-theme/build/banner.asset.php
+++ wp-content/themes/fictional-block-theme/build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '7fe3f4e6551327393949');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '4942ec65ab635e6c0095');
