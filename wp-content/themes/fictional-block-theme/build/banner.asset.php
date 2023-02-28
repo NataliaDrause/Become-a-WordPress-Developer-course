a0f4423b754f6614225931a72410e7416897f48b
NataliaDrause <nataliadrause@gmail.com>
2023-02-28 11:31:47 +0200

    add PHP render approach for the banner block

--- wp-content/themes/fictional-block-theme/build/banner.asset.php
+++ wp-content/themes/fictional-block-theme/build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '094fdd8295b54f5c3b6a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '72567c139e0cf3b78123');
